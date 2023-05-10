95655177eadd3316e7fd948c7ef5301d585ac372
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-10 12:01:58 +0900

    add 7

--- src/bin/007.rs
+++ src/bin/007.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: isize,
+        x: isize,
+        y: isize
+    }
+
+    let mut sum = 0;
+
+    for i in 1..=n {
+        if i % x == 0 || i % y == 0 {
+            sum += 1;
+        };
+    }
+
+    println!("{}", sum);
+
 }
