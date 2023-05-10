cea76bcb5c5b13e9a2792d46a3b996d336216515
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-10 23:47:01 +0900

    update 11

--- src/bin/011.rs
+++ src/bin/011.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: isize
+    };
+
+    let mut answer_vector = Vec::<i32>::new();
+
+    for i in 2..=n {
+        if is_prime(i as i32) {
+            answer_vector.push(i as i32);
+        }
+    }
+
+
+    for i in answer_vector.iter() {
+        print!("{} ", i);
+    }
+
+}
+
+fn is_prime(target: i32) -> bool {
+    for i in 2..target {
+        if target % i == 0 {
+            return false;
+        }
+    }
+
+    return true;
 }
