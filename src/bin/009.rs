30305e4e089137762916c91919bade025b33fed3
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-10 15:02:47 +0900

    add 009

--- src/bin/009.rs
+++ src/bin/009.rs
@@ -1,3 +1,37 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: isize,
+        s: isize,
+        _a: [isize; n]
+    };
+
+    let a = _a.to_vec();
+
+    let mut answer = false;
+
+    for i in 0..1<<n {
+        let mut sum = 0;
+        for j in 0..n {
+
+            if i & 1 << j != 0 {
+                sum += a[j as usize]
+            }
+
+        }
+
+        if sum == s {
+            answer = true;
+            break;
+        }
+
+    }
+
+    if answer {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+
 }
