ad0312fcad2ddb6e582639c5569ed28769427701
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-10 12:08:16 +0900

    008

--- src/bin/008.rs
+++ src/bin/008.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: isize,
+        s: isize
+    };
+
+    let mut answer = 0;
+
+    for i in 1..=n {
+        for j in 1..=n {
+            if i + j <= s {
+                answer += 1;
+            }
+        }
+    }
+
+    println!("{}", answer);
+
 }
