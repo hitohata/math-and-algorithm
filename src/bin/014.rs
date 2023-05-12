c6fb8af6345111920711b025db5111c3ab81ddb3
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-12 10:36:06 +0900

    add 14

--- src/bin/014.rs
+++ src/bin/014.rs
@@ -1,3 +1,42 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: isize
+    }
+
+    let mut primes = (2..=n)
+        .take_while(|&j| j * j <=n)
+        .filter(|j| n % j == 0)
+        .map(|j| vec![j, n / j])
+        .flatten()
+        .collect::<Vec<_>>();
+
+    primes.sort();
+    primes.dedup();
+
+    let mut i = 0;
+    let mut divided = n;
+    let mut answer = Vec::<isize>::new();
+
+    loop {
+        if divided % primes[i] != 0 {
+            i += 1;
+            continue;
+        }
+
+        answer.push(primes[i]);
+
+        divided = divided % primes[i];
+
+        if primes.contains(&divided) {
+            answer.push(divided);
+            break;
+        }
+    }
+
+    for i in answer {
+        println!("{}", i);
+    }
+
 }
