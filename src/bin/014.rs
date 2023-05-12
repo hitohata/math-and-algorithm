ccb94102cf9319b50cc85af844f4af73c0a5ce6d
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-12 10:41:27 +0900

    update 14

--- src/bin/014.rs
+++ src/bin/014.rs
@@ -5,38 +5,22 @@ fn main() {
         n: isize
     }
 
-    let mut primes = (2..=n)
-        .take_while(|&j| j * j <=n)
-        .filter(|j| n % j == 0)
-        .map(|j| vec![j, n / j])
-        .flatten()
-        .collect::<Vec<_>>();
-
-    primes.sort();
-    primes.dedup();
-
-    let mut i = 0;
-    let mut divided = n;
     let mut answer = Vec::<isize>::new();
+    let mut divided = n;
 
-    loop {
-        if divided % primes[i] != 0 {
-            i += 1;
-            continue;
-        }
-
-        answer.push(primes[i]);
-
-        divided = divided % primes[i];
-
-        if primes.contains(&divided) {
-            answer.push(divided);
-            break;
+    for i in (2..=n).take_while(|&j| j * j <=n) {
+        while divided % i == 0 {
+            answer.push(i);
+            divided /= i;
         }
     }
 
+    if divided != 1 {
+        answer.push(divided);
+    }
+
     for i in answer {
-        println!("{}", i);
+        print!("{} ", i);
     }
 
 }
