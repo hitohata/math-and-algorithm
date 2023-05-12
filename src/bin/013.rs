5f4f0a0997497e56446cede2c3e6e51abbe1692a
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-12 09:53:22 +0900

    update 13

--- src/bin/013.rs
+++ src/bin/013.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: i64
+    }
+
+    let mut answer = Vec::<i64>::new();
+    let mut i = 1;
+
+    while i * i <= n {
+
+        if n % i == 0 {
+            answer.push(i);
+            answer.push(n/i);
+        }
+        i += 1;
+    };
+
+    answer.sort();
+    answer.dedup();
+
+    for a in answer {
+        println!("{}", a);
+    }
+
 }
