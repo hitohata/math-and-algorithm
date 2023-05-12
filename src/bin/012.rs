17ff5e7ff7fa40be33b4c4b563631f9f0008e30a
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-12 09:41:29 +0900

    add 12

--- src/bin/012.rs
+++ src/bin/012.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        n: isize
+    };
+
+    let mut i = 2;
+    let mut answer = true;
+
+    while i * i <= n {
+        if n % i == 0 {
+            answer = false;
+            break;
+        }
+        i += 1;
+    }
+
+    if answer {
+        println!("{}", "Yes")
+    } else {
+        println!("{}", "No")
+    }
 }
