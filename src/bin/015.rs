484ba82e7820cb9d9a14cb7f0ff29f39b6de3bbd
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-14 16:47:51 +0900

    add 15

--- src/bin/015.rs
+++ src/bin/015.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: isize,
+        b: isize
+    }
+
+    if a > b {
+        println!("{}", gcd(a, b))
+    } else {
+        println!("{}", gcd(b, a))
+    }
+
+}
+
+fn gcd(numerator: isize, denominator: isize) -> isize {
+
+    let mod_value = numerator % denominator;
+
+    if mod_value == 0 {
+        return denominator;
+    }
+
+    return gcd(denominator, mod_value);
+
 }
