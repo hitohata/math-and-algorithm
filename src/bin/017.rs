ee9f6c4f091cdec03b3cd89bc892d61a605ad28e
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-14 17:35:21 +0900

    add 17

--- src/bin/017.rs
+++ src/bin/017.rs
@@ -1,3 +1,45 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut answer = a[0];
+
+    for i in 1..n {
+        answer = lcd(answer, a[i])
+    }
+
+    println!("{}", answer);
+
+}
+
+fn lcd(a: usize, b: usize) -> usize {
+
+    return a / gcd(a, b) * b;
+
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+
+    if a > b {
+        let mod_val = a % b;
+
+        if mod_val == 0 {
+            return b;
+        }
+
+        return gcd(a % b, b);
+    } else {
+        let mod_val = b % a;
+
+        if mod_val == 0 {
+            return a;
+        }
+
+        return gcd(a, mod_val)
+    }
 }
