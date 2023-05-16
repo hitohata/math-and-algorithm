2e154928ca899dd5489af7305db25f6138eac6b3
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-16 12:01:39 +0900

    add 19

--- src/bin/019.rs
+++ src/bin/019.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut answer = 0;
+    let mut cards = [0usize; 4];
+    a.into_iter().for_each(|el| cards[el] += 1);
+
+    for i in 1..=3 {
+
+        if cards[i] < 2 {
+            continue;
+        };
+
+        answer += cards[i] * (cards[i] - 1) / 2;
+    }
+
+    println!("{}", answer);
+
 }
