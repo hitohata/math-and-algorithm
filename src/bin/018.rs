b28cbfdb8eee900739227495bf37b2affbe275d5
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-16 11:44:17 +0900

    add 18

--- src/bin/018.rs
+++ src/bin/018.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut ans = vec![0usize; 5];
+    a.iter().for_each(|el| ans[el / 100] += 1);
+
+    println!("{}", ans[1] * ans[4] + ans[2] * ans[3])
+
 }
