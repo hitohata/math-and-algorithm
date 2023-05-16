571b074603b8238902a57f648973e20daf150621
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-16 16:27:09 +0900

    add 21

--- src/bin/021.rs
+++ src/bin/021.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        r: usize
+    }
+
+
+    let mut numerator = 1;
+    let mut denominator = 1;
+
+    for i in 0..r {
+        numerator *= n - i;
+        denominator *= r - i;
+    }
+
+    println!("{}", numerator / denominator);
+
 }
