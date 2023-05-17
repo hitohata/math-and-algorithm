564b1bd52ec7f7142806e105564dae06e0ce7b0d
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-17 17:59:49 +0900

    add 24

--- src/bin/024.rs
+++ src/bin/024.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        pq: [[f64; 2]; n],
+    }
+
+    let mut expected_value_sum: f64 = 0_f64;
+
+    for i in 0..n {
+        expected_value_sum += pq[i][1] / pq[i][0];
+    }
+
+    println!("{0:.12}", expected_value_sum);
+
 }
