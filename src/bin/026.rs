2339283d052aed1bf1b80192353d629319532d90
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-18 08:02:11 +0900

    add 26

--- src/bin/026.rs
+++ src/bin/026.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize
+    }
+
+    let answer = (1..=n).map(|x| 1.0 / x as f64).sum::<f64>();
+
+    println!("{0:.12}", answer * n as f64);
+
 }
