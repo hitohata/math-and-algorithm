bba5203c687d518d5a346055ce0d0e9e7043ef74
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-17 18:12:39 +0900

    add 25

--- src/bin/025.rs
+++ src/bin/025.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [f64; n],
+        b: [f64; n]
+    }
+
+    let a_possibility: f64 = 2.0 / 6.0;
+    let b_possibility: f64 = 4.0 / 6.0;
+
+    let answer = (0..n).map(|i| a_possibility * a[i] + b_possibility * b[i]).sum::<f64>();
+
+    println!("{0:.12}", answer);
+
 }
