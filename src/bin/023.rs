5f02d22c1663f7312c5a49314a977272692893c4
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-17 17:47:10 +0900

    add 23

--- src/bin/023.rs
+++ src/bin/023.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        b: [usize; n],
+        r: [usize; n]
+    }
+
+    let b_sum = b.into_iter().fold(0, |acc, x| acc + x) as f64;
+    let r_sum = r.into_iter().fold(0, |acc, x| acc + x) as f64;
+
+    println!("{}", b_sum / n as f64 + r_sum / n as f64)
+
 }
