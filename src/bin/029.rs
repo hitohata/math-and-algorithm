4bb959cb0650a27fb89eed6328d2479881f6fb3a
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-18 14:07:49 +0900

    add 29

--- src/bin/029.rs
+++ src/bin/029.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize
+    }
+
+    let mut dp = vec![0_usize; n + 1];
+
+    for i in 0..=n {
+
+        if i < 2 {
+            dp[i] = 1;
+        } else {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+    }
+
+    println!("{}", dp[n]);
+
 }
