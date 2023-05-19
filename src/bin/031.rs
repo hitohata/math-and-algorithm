0571b602aafb8c72eea6277293373388e687f9e6
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-19 17:14:28 +0900

    add 31

--- src/bin/031.rs
+++ src/bin/031.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut dp1 = vec![0_usize; n + 1];
+    let mut dp2 = vec![0_usize; n + 1];
+
+    for i in 1..=n {
+        dp1[i] = dp2[i - 1] + a[i - 1];
+        dp2[i] = std::cmp::max(dp1[i - 1], dp2[i - 1]);
+
+    }
+
+    println!("{}", std::cmp::max(dp1[n], dp2[n]));
+
 }
