7566e952fa03c5d589c2865a322c32a21caa3fc8
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-19 19:06:56 +0900

    add 32

--- src/bin/032.rs
+++ src/bin/032.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        x: usize,
+        _a: [usize; n]
+    }
+
+    let mut l = 0;
+    let mut r = n;
+    let mut is_exists = false;
+
+    let mut a = _a.to_vec();
+    a.insert(0, 0);
+
+    a.sort();
+
+    while l <= r {
+
+        let m = (l + r) / 2;
+
+        if a[m] == x {
+            is_exists = true;
+            break;
+        }
+
+        if a[m] > x {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    println!("{}", if is_exists {"Yes"} else {"No"})
+
 }
