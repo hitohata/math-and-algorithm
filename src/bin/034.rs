22713ea625fe771c1a9b7a75cdc5557fd6cbf582
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-20 11:34:51 +0900

    add 34

--- src/bin/034.rs
+++ src/bin/034.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        xy: [(isize, isize); n]
+    }
+
+    let mut smallest: f64 = std::f64::MAX;
+
+    for i in 0..n - 1 {
+        for j in i+1..n {
+            smallest = if smallest < distance(xy[i], xy[j]) {
+                smallest
+            } else {
+                distance(xy[i], xy[j])
+            }
+        }
+    }
+
+    println!("{0:.12}", smallest);
+
+}
+
+fn distance(a: (isize, isize), b: (isize, isize)) -> f64 {
+    return f64::sqrt(((a.0 - b.0).pow(2) + (a.1 - b.1).pow(2)) as f64);
 }
