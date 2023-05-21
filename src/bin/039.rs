406d65c1ad24ea21d8338fc9c4d8989823c63bb6
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-21 21:23:48 +0900

    add 39 wip

--- src/bin/039.rs
+++ src/bin/039.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        q: usize
+    }
+
+    let mut snow = vec![0_isize; n + 1];
+
+    for _ in 1..=q {
+        input! {
+            l: isize,
+            r: isize,
+            x: isize
+        }
+
+        for j in l -1..r {
+            snow[j as usize] += x;
+        }
+
+    }
+
+    for i in 1..n {
+        let result = snow[i - 1] - snow[i];
+
+        if result < 0 {
+            print!("<");
+        }
+        else if result > 0 {
+            print!(">");
+        }
+        else {
+            print!("=")
+        }
+    }
+
 }
