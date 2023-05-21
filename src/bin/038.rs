7e6531f123d9109369f9d4c7dac332abc3228b5c
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-21 21:06:42 +0900

    add 38

--- src/bin/038.rs
+++ src/bin/038.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize; n],
+    }
+
+    let mut total = vec![0; n + 1];
+
+    for i in 0..n {
+        total[i + 1] = total[i] + a[i];
+    }
+
+    for _ in 0..q {
+
+        input! {
+            l: usize,
+            r: usize
+        }
+
+        println!("{}", total[r] - total[l - 1]);
+
+    }
+
 }
