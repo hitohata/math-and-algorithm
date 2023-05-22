91ebbaa1f26e5aa9b653ede7b6b4edfc02199c98
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-22 19:48:42 +0900

    add 40

--- src/bin/040.rs
+++ src/bin/040.rs
@@ -1,3 +1,41 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [isize; n - 1],
+        m: usize
+    }
+
+    let mut dist = a.to_vec();
+    dist.insert(0, 0);
+
+    for i in 0..(n - 1) {
+        dist[i + 1] += dist[i];
+    };
+
+    let mut answer = 0;
+
+    let mut current_station = 0;
+
+    for i in 0..m {
+        input! {
+            b: usize
+        }
+
+        if i == 0 {
+            current_station = b;
+            continue;
+        }
+
+        let d = isize::abs(dist[b - 1] - dist[current_station - 1]);
+
+        answer += d;
+        current_station = b;
+
+    }
+
+    println!("{}", answer);
+
 }
