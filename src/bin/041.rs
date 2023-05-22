f05a558cd3ee38f90d89268b54cb10510cc0b314
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-22 20:19:05 +0900

    add 41

--- src/bin/041.rs
+++ src/bin/041.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        t: usize,
+        n: usize,
+    }
+
+    let mut w = vec![0_isize; t + 2];
+
+    for _ in 0..n {
+
+        input! {
+            l: usize,
+            r: usize
+        }
+
+        w[l] += 1;
+        w[r] -= 1;
+
+    }
+
+    let mut s: isize = 0;
+
+    for i in 0..t {
+        if i == 0 {
+            s = w[0];
+        } else {
+            s += w[i]
+        }
+        println!("{}", s);
+    }
 }
