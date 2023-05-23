963220c0c15996cc2dbfc90f0b68a0c6d6b822c5
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-23 18:28:36 +0900

    add 42

--- src/bin/042.rs
+++ src/bin/042.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize
+    }
+
+    let mut answer = 0;
+
+    let mut f = vec![0_usize; n + 1];
+
+    for i in 1..=n {
+        for j in (i..=n).step_by(i) {
+            f[j] += 1;
+        }
+    }
+
+    for i in 1..=n {
+        answer += i * f[i];
+    }
+
+    println!("{}", answer);
 }
