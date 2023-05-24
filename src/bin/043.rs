827811c868162fc84352dd8214cf3098275218d9
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-24 20:25:31 +0900

    add 43

--- src/bin/043.rs
+++ src/bin/043.rs
@@ -1,3 +1,50 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        m: usize
+    }
+
+    let mut answer = true;
+
+    let mut visited = vec![false; n + 1];
+    let mut graph = vec![vec![]; n + 1];
+
+    (1..=m).for_each(|_| {
+        input! {
+            a: usize,
+            b: usize
+        }
+
+        graph[a].push(b);
+        graph[b].push(a);
+
+    });
+
+    dfs(1, &graph, &mut visited);
+
+    for i in 1..=n {
+        if !visited[i] {
+            answer = false;
+            break;
+        }
+    }
+
+    println!("{}", if answer {"The graph is connected."} else {"The graph is not connected."} )
+
+}
+
+fn dfs(position: usize, graph: &Vec<Vec<usize>>, visited: &mut Vec<bool>) {
+    visited[position] = true;
+
+    graph[position].iter().for_each(|to| {
+
+        if !visited[to.to_owned()] {
+            dfs(to.to_owned(), graph, visited)
+        }
+
+    });
+
 }
