be0c5be9f96f51f4c6a57c7a2ad87cd3919c95eb
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-25 12:41:37 +0900

    add 45

--- src/bin/045.rs
+++ src/bin/045.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        m: usize
+    }
+
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; n + 1];
+
+    for _ in 0..m {
+
+        input! { a: usize, b: usize }
+
+        graph[a].push(b);
+        graph[b].push(a);
+
+    }
+
+    let mut answer = 0;
+
+    for i in 1..=n {
+
+        let mut count = 0;
+
+        for j in 0..graph[i].len() {
+            if graph[i][j] < i {
+                count += 1
+            }
+        }
+
+        if count == 1 { answer += 1 };
+
+    }
+
+    println!("{}", answer);
+
 }
