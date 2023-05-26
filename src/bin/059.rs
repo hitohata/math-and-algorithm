4a3c96591f4d75fa23a7a3a605e0e077838c32ac
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-26 17:01:20 +0900

    add 59

--- src/bin/059.rs
+++ src/bin/059.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize
+    }
+
+    println!("{}", match n % 4 {
+        1 => 2,
+        2 => 4,
+        3 => 8,
+        _ => 6
+    })
+
 }
