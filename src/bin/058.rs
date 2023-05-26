cbc5cc3b2463f8ec1371b2ddccd3d89d64ea9e71
hirohata <43802663+hitohata@users.noreply.github.com>
2023-05-26 16:37:42 +0900

    add 58

--- src/bin/058.rs
+++ src/bin/058.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: isize,
+        x: isize,
+        y: isize
+    }
+
+    if x.abs() + y.abs() <= n && (x + y) % 2 == n % 2 {
+        println!("Yes");
+    } else {
+        println!("No")
+    }
+
 }
