6f24acdaf39774e528f09916f92e39376c70f65c
hirohat <hirohatatro@gmail.com>
2023-05-31 17:26:05 +0900

    update 061

--- src/bin/061.rs
+++ src/bin/061.rs
@@ -6,7 +6,15 @@ fn main() {
         n: usize
     }
 
-    if n % 3 == 0 {
+    let mut answer = false;
+
+    for i in 1..=60 {
+        if n == (1 << i) - 1 {
+            answer = true;
+        }
+    }
+
+    if answer {
         println!("Second");
     } else {
         println!("First");
