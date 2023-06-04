d9176ea2587733774cf2daf377d4982d828747f3
hirohat <hirohatatro@gmail.com>
2023-06-04 11:08:10 +0900

    add 64

--- src/bin/064.rs
+++ src/bin/064.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+
+    let sum: usize = a.to_vec().iter().sum();
+
+    if k < sum {
+        println!("No");
+        return;
+    }
+
+    if k == sum {
+        println!("Yes");
+        return;
+    }
+
+    if (k - sum) % 2 == 0 {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+
 }
