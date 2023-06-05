2ea1b9c524c67c902a2025d89b7e434bd796c775
hirohat <hirohatatro@gmail.com>
2023-06-05 18:33:16 +0900

    add 68

--- src/bin/068.rs
+++ src/bin/068.rs
@@ -1,3 +1,49 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        k: usize,
+        v: [usize; k]
+    }
+
+    let mut answer = 0;
+
+    for i in 1..1<<k {
+
+        let mut count = 0;
+        let mut latest_common_multiple = 1;
+
+        for j in 0..k {
+            if (i & 1 << j) != 0 {
+                count += 1;
+                latest_common_multiple = lcm(latest_common_multiple, v[j]);
+            }
+        }
+
+        let num = n / latest_common_multiple;
+
+        if count % 2 == 1 {
+            answer += num;
+        } else {
+            answer -= num;
+        };
+
+    }
+
+    println!("{}", answer);
+
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+
+    return gcd(b, a % b);
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    (a / gcd(a, b)) * b
 }
