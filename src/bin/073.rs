7362768aae39eb4e5a39d3f0882bd7650f257508
hirohat <hirohatatro@gmail.com>
2023-06-07 13:39:11 +0900

    add 73

--- src/bin/073.rs
+++ src/bin/073.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
+const MOD: usize = 1000000007;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a: [usize; n]
+    }
+
+    let mut answer = 0;
+    let mut s = 1;
+
+    for i in 0..n {
+        answer += a[i] * s;
+        answer %= MOD;
+        s *= 2;
+        s %= MOD;
+
+    }
+
+    println!("{}", answer);
+
 }
