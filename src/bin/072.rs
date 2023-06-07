179d487b98bd710da1987333d4620fcb67ff6636
hirohat <hirohatatro@gmail.com>
2023-06-07 12:19:23 +0900

    add 72

--- src/bin/072.rs
+++ src/bin/072.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    let mut answer = 0;
+
+    for i in 1..=b {
+        if check(&a, &b, i) {
+            answer = i;
+        }
+    }
+
+    println!("{}", answer);
+
+}
+
+#[inline]
+fn check(a: &usize, b: &usize, t: usize) -> bool {
+    let left = ((a + t - 1) / t) as isize;
+    let right = (b / t) as isize;
+
+    if right - left >= 1 {
+        true
+    } else {
+        false
+    }
 }
