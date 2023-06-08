1618584ce80462a5fd6f813d838e8aa8921afae1
hirohat <hirohatatro@gmail.com>
2023-06-08 17:45:14 +0900

    add 49

--- src/bin/049.rs
+++ src/bin/049.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize
+    }
+
+    let mut fib = [1, 1];
+
+    for _ in 2..n {
+        let sum = (fib[0] + fib[1]) % MOD;
+        fib[0] = fib[1];
+        fib[1] = sum;
+    }
+
+    println!("{}", fib[1]);
+
 }
