290afd9f2b67767602fdf8572ebb99c9482592d5
hirohat <hirohatatro@gmail.com>
2023-06-08 17:55:51 +0900

    add 50

--- src/bin/050.rs
+++ src/bin/050.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
+const MOD: usize = 1_000_000_007;
+
 fn main() {
-    todo!();
+
+    input! {
+        a: usize,
+        b: usize
+    }
+
+    println!("{}", mod_pow(a, b))
+}
+
+fn mod_pow(a: usize, b: usize) -> usize {
+
+    let mut answer = 1;
+    let mut p = a;
+
+    for i in 0..30 {
+        if (b & 1 << i) != 0 {
+            answer *= p;
+            answer %= MOD;
+        }
+        p *= p;
+        p %= MOD;
+    }
+
+    answer
 }
