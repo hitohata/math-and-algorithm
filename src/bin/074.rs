70705456564792155727aff9face4cb845d1cfea
hirohat <hirohatatro@gmail.com>
2023-06-12 14:25:41 +0900

    add 074

--- src/bin/074.rs
+++ src/bin/074.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+
+    input! {
+        n: usize,
+        a_in: [usize; n]
+    }
+
+    let mut a = a_in.to_vec();
+    a.insert(0, 0);
+
+    let sum = (0..=n).map(|i| (a[i] as isize) * (2 * (i as isize ) - 1 - n as isize)).sum::<isize>();
+
+    println!("{}", sum);
+
 }
